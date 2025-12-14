50e1fafc721e4ecbe79ed686d686afd19ffff3d8
wasp <wasp123321@gmail.com>
2025-12-14 14:11:55 +0100

    Exercise 9.2 in progress

---
+++ part9/exerciseCalculator.ts
@@ -0,0 +1,10 @@
+
+interface CalculatorResult {
+  periodLength: number;
+  trainingDays: number;
+  success: boolean;
+  rating: number;
+  ratingDescription: string;
+  target: number;
+  average: number
+}
